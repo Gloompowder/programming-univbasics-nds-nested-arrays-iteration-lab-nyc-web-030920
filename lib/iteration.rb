088b4c1c8cfc75ca71d33581197fb4fd66518061
William Lin <willlin1996@gmail.com>
2020-02-27 21:47:44 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -45,6 +45,7 @@ def total_even_pairs(src)
   row_index = 0 
   even_array = []
   while row_index < src.count do 
-    if (src[row_index][0] + src[row_index][1]) % 2 == 0
+    if ((src[row_index][0] + src[row_index][1]) % 2) == 0
+      even_array << src[row_index][0] + src[row_index][1]
   row_index += 1
 end
